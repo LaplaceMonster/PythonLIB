87f9c0d5194b62b669bd464f0079cc949a7ee736
LCB <lcb19990128@gmail.com>
2025-10-12 22:12:13 +0800

    更新2025年2025年10月12日

--- 图像绘制/2D.py
+++ 图像绘制/2D.py
@@ -24,19 +24,38 @@ python绘制2D图像非常简单 只需要三步
 '''
 # 生成数据
 x = numpy.linspace(0, 10, 100)
-y = numpy.sin(x)
-
+y1 = numpy.sin(x)
+y2 = numpy.cos(x)
+y3=numpy.tan(x)
 # 创建图形
-plt.plot(x, y, label="sin(x)", color="blue", linestyle="--", linewidth=2)
-
+plt.plot(x, y1, label="sin(x)", color="blue", linestyle="--", linewidth=2)
+plt.plot(x, y2, label="cos(x)", color="red", linestyle="-.", linewidth=2)
+plt.plot(x, y3, label="tan(x)", color="green", linestyle="-", linewidth=2)# 可以在同一个图中绘制多条曲线
 # 添加标题和标签
-plt.title("正弦函数")
+plt.title("正弦与余弦函数")
 plt.xlabel("x 轴")
 plt.ylabel("y 轴")
 
-# 显示图例:在图中添加曲线的名称
-plt.legend(loc='upper right')  # loc参数可以指定图例的位置，如'upper right', 'lower left'等
-plt.grid(True)  # 添加网格线
+# 显示图例
+plt.legend(loc='upper right')
+plt.grid(True)
+
 # 显示图形
 plt.show()
 #plt.savefig("picture", dpi=300)  # 保存为 PNG 格式，300 dpi，确保清晰
+
+# 创建 1行2列 的子图
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))  # figsize可调整整体大小
+axes[0].plot(x, y1, label="sin(x)", color="blue", linestyle="--", linewidth=2)
+axes[0].legend(loc='upper right')
+axes[0].grid(True)
+axes[0].set_title("正弦函数")
+axes[0].set_xlabel("x 轴")
+axes[0].set_ylabel("y 轴")
+axes[1].plot(x, y2, label="cos(x)", color="red", linestyle="-.", linewidth=2)
+axes[1].legend(loc='upper right')
+axes[1].grid(True)
+axes[1].set_title("余弦函数")
+axes[1].set_xlabel("x 轴")
+axes[1].set_ylabel("y 轴")
+plt.show()
